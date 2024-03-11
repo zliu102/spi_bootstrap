b023a2c624887e9974dd18530841fa50be85a8cc
zliu102 <zliu102@hawk.iit.edu>
2024-03-11 15:55:54 +0800

    a

--- spi_bootstrap.c
+++ spi_bootstrap.c
@@ -250,9 +250,9 @@ Datum spi_bootstrap2(PG_FUNCTION_ARGS) {
         values[0] = group->l_suppkey;
         values[1] = group->l_returnflag_int;
         values[2] = avg_quantity;
-        elog(INFO, "l_suppkey is %f",values[0]);
-        elog(INFO, "l_returnflag_int is %f",values[1]);
-        elog(INFO, "avg_quantity is %f",values[2]);
+        elog(INFO, "l_suppkey is %f",group->l_suppkey;);
+        elog(INFO, "l_returnflag_int is %f",group->l_returnflag_int);
+        elog(INFO, "avg_quantity is %f",avg_quantity);
 
         tuplestore_putvalues(tupstore, tupdesc, values, nulls);
     }
