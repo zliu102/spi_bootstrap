65cc148a8dd28d99f3fd164b41f10ed84a0405ab
zliu102 <zliu102@hawk.iit.edu>
2024-03-11 15:02:11 +0800

    a

--- spi_bootstrap.c
+++ spi_bootstrap.c
@@ -201,6 +201,20 @@ Datum spi_bootstrap2(PG_FUNCTION_ARGS) {
         HeapTuple tuple = SPI_tuptable->vals[i];
         TupleDesc tupdesc = SPI_tuptable->tupdesc;
         elog(INFO, "SPI current id is -- %d", i);
+
+        int attnum1 = SPI_fnumber(SPI_tuptable->tupdesc, "l_suppkey");
+        int attnum2 = SPI_fnumber(SPI_tuptable->tupdesc, "l_returnflag_int");
+        int attnum3 = SPI_fnumber(SPI_tuptable->tupdesc, "l_quantity");
+        char* value1 = SPI_getvalue((SPI_tuptable->vals)[row], SPI_tuptable->tupdesc, attnum1);
+        char* value2 = SPI_getvalue((SPI_tuptable->vals)[row], SPI_tuptable->tupdesc, attnum2);
+        char* value3 = SPI_getvalue((SPI_tuptable->vals)[row], SPI_tuptable->tupdesc, attnum3);
+        elog(INFO, "SPI l_suppkey -- %d", attnum1);
+        elog(INFO, "SPI l_returnflag_int -- %d", attnum2);
+        elog(INFO, "SPI quantity -- %d", attnum3);
+        elog(INFO, "SPI l_suppkey -- %s", value1);
+        elog(INFO, "SPI l_returnflag_int -- %s", value2);
+        elog(INFO, "SPI quantity -- %s", value3);
+
         int l_suppkey = DatumGetInt32(SPI_getbinval(tuple, tupdesc, 1, NULL));
         int l_returnflag_int = DatumGetInt32(SPI_getbinval(tuple, tupdesc, 2, NULL));
         int quantity = DatumGetInt32(SPI_getbinval(tuple, tupdesc, 3, NULL));
