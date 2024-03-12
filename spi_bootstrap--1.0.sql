d9646d2cc39afef0605d8cc7c48023199a7f8a54
zliu102 <zliu102@hawk.iit.edu>
2024-03-12 14:30:23 +0800

    a

--- spi_bootstrap--1.0.sql
+++ spi_bootstrap--1.0.sql
@@ -43,7 +43,7 @@ LANGUAGE C STRICT;
 */
 
 CREATE FUNCTION spi_bootstrap2(sampleSize text, tablename text, otherAttribue text,groupby text) 
-RETURNS TABLE (l_suppkey int, l_returnflag_int int, avg_l_quantity int) 
+RETURNS TABLE (l_suppkey int, l_returnflag_int int, avg_l_quantity float4) 
 AS 'MODULE_PATHNAME','spi_bootstrap2' 
 LANGUAGE C STRICT;
 
